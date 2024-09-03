ee56796dfc625d561807c19081bf50702ea148c2
Ranetanmaydasharath <tanmayrane313@gmail.com>
2024-09-03 14:58:53 +0530

    first commit

---
+++ src/main/java/com/tka/OrganizationWorkflow/OrganizationWorkflowApplication.java
@@ -0,0 +1,13 @@
+package com.tka.OrganizationWorkflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganizationWorkflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganizationWorkflowApplication.class, args);
+	}
+
+}
